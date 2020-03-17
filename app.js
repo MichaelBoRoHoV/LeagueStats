3997db68a26a7f3ce461b76e38606344cee7c924
MichaelBoRoHoV <mikelb138@gmail.com>
2020-03-17 20:39:14 +0200

    Hello World

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+const express = require("express")
+
+const app = express();
+
+app.get("/",function(req, res){
+    res.send("Hello World!")
+});
+
+
+app.listen(3000,function(){
+    console.log("Server is up and running in port 3000")
+});
